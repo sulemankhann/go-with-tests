7ba1aeec9b64d065b0a6ddd18e1c343682c0c6e0
Suleman Khan <sulemank97@gmail.com>
2024-01-16 09:37:58 +0500

    write test case tot est sleep before printing every coun

--- mocking/main.go
+++ mocking/main.go
@@ -10,18 +10,25 @@ import (
 const (
 	finalWord      = "Go!"
 	countdownStart = 3
+	write          = "write"
+	sleep          = "sleep"
 )
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
 
 type Sleeper interface {
 	Sleep()
-}
-
-type SpySleeper struct {
-	Calls int
-}
-
-func (s *SpySleeper) Sleep() {
-	s.Calls++
 }
 
 type DefaultSleeper struct{}
